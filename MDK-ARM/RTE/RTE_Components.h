f4d7cd8ddd8cbccc8c9de57c5aba9240ab5e4544
calebkang <kangcaleb@gmail.com>
2020-10-29 15:48:29 +0900

    init

---
+++ MDK-ARM/RTE/RTE_Components.h
@@ -0,0 +1,14 @@
+
+/*
+ * Auto generated Run-Time-Environment Component Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'stm32g431_esd_test_project_uart_spi' 
+ * Target:  'stm32g431_esd_test_project_uart_spi' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+#endif /* RTE_COMPONENTS_H */
